12e1cbe2cde24357628c80aee57253fda98aaf42
Oleg Shatov <oleq.shatov@yandex.ru>
2026-01-18 09:30:24 +0100

    Tsan annotations in epoll

--- lib/proto-coro/event-loop/epoll.cpp
+++ lib/proto-coro/event-loop/epoll.cpp
@@ -6,6 +6,27 @@
 #include <sys/epoll.h>
 #include <sys/eventfd.h>
 
+#if __has_feature(thread_sanitizer)
+extern "C" {
+    void __tsan_acquire(void* ptr);
+    void __tsan_release(void* ptr);
+}
+
+void Acquire(void* ptr) {
+    __tsan_acquire(ptr);
+}
+
+void Release(void* ptr) {
+    __tsan_release(ptr);
+}
+#else
+void Acquire(void*) {
+}
+
+void Release(void*) {
+}
+#endif
+
 Epoll::Epoll() {
     int efd = epoll_create1(EPOLL_CLOEXEC);
     if (efd < 0) {
@@ -25,10 +46,12 @@ Epoll::Epoll() {
 }
 
 int Epoll::Register(int fd, uint32_t flags, void* cookie) {
+    Release(cookie);
     return Change(fd, flags, cookie, EPOLL_CTL_ADD);
 }
 
 int Epoll::Modify(int fd, uint32_t flags, void* cookie) {
+    Release(cookie);
     return Change(fd, flags, cookie, EPOLL_CTL_MOD);
 }
 
@@ -66,6 +89,7 @@ Epoll::Poll(int timeout_ms, std::span<std::pair<uint32_t, void*>> tasks_buf) {
             --tasks;
             continue;
         }
+        Acquire(ptr);
         tasks_buf.front() = {event.events, ptr};
         tasks_buf = tasks_buf.subspan(1);
     }
